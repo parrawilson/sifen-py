828626a5b67ea886d5f949176aba1070462c6c2b
parrawilson <wilsonccont@gmail.com>
2025-06-25 14:12:37 -0300

    Fix package structure

--- setup.py
+++ setup.py
@@ -2,11 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="sifen",
-    version="0.1",
-    packages=find_packages(),
+    version="0.2.0",
+    packages=find_packages(include=['sifen', 'sifen.*']),
+    include_package_data=True,
     install_requires=[
-        "lxml>=4.6.0",
-        "pyOpenSSL>=20.0.0",
-        "python-dotenv>=0.19.0"  # Para manejar variables de entorno
+        'lxml>=4.6.0',
+        'pyOpenSSL>=20.0.0',
+        'python-dotenv>=1.0.0'
     ],
+    package_data={
+        'sifen': ['schemas/*.xsd', 'certs/dev/*.pem'],
+    },
 )
